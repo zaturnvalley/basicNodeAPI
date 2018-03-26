cc08d94e95cd64c60b621d802a182164f1ba9b05
zaturnvalley <zacharycmason@gmail.com>
2018-03-26 15:23:47 -0700

    added routes and index file for app, testing post route

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const port = 8000;
+
+require('./app/routes')(app, {});
+
 app.listen(port, () => {
   console.log("We are live on " + port);
 });
