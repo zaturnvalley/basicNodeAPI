e618347c66d616043ac906777725afd3ee132a21
zaturnvalley <zacharycmason@gmail.com>
2018-03-26 17:54:00 -0700

    added delete route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -27,4 +27,18 @@ module.exports = function(app, db) {
       }
     });
   });
+
+  // Delete Route
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+
+    const details = {'_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': err })
+      } else {
+        res.send('Note' + id + ' deleted');
+      }
+    })
+  });
 }
