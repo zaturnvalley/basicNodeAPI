531e8659a85348f65f7b7af2a6673ce01e69d14f
zaturnvalley <zacharycmason@gmail.com>
2018-03-26 17:51:45 -0700

    added get route to fetch from db

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,12 +1,29 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+  // Get Route
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+
+    const details = {'_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': err })
+      } else {
+        res.send(item);
+      }
+    })
+  });
+
+  // Post Route
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
 
     db.collection('notes').insert(note, (err, results) => {
       if (err) {
-        res.send({ 'error': 'An error has occured' });
+        res.send({ 'error': err });
       } else {
-        res.send(result.ops[0]);
+        res.send(results.ops[0]);
       }
     });
   });
